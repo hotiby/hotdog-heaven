a06b8de523ce436f812d05a25f0258076f2c16f8
Matt Elhotiby <hotiby@gmail.com>
2008-09-13 19:41:08 -0400

    second commit

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  #this is my change
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '4f11d2b758dc0bcb936ac4e385b3519f'
